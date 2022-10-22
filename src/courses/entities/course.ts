f8a2188bb7d8e4e3ee4618b9e4ed1100ae69bc74
Guillermo Jimenez <jimenezguillermo1208@gmail.com>
2022-10-22 19:47:06 -0400

    lo mismo

--- src/courses/entities/course.ts
+++ src/courses/entities/course.ts
@@ -1,5 +1,5 @@
-import { student } from 'src/person/entities/student';
-import { statecourse } from './statecourse';
+import { Student } from 'src/person/entities/student';
+import { StateCourse } from './statecourse';
 import { Lesson } from 'src/Lesson/Entities/Lesson';
 export class Course {
   id: number;
@@ -8,6 +8,6 @@ export class Course {
   lessons: Lesson[] = [];
   categories: string; // Should be an Category[] in the future
   keywords: string;
-  state: statecourse;
-  students: student[];
+  state: StateCourse;
+  students: Student[];
 }
