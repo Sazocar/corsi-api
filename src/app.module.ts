87a3016ddd55158d1e88b84e8e04b4e9f8a0e6d0
Guillermo Jimenez <jimenezguillermo1208@gmail.com>
2022-10-22 13:37:14 -0400

    Creo carpeta lecciones, entidad lecciones y controlador

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CourseModule } from './courses/course.module';
+import { ControllersController } from './lesson/controllers/controllers.controller';
 
 @Module({
   imports: [CourseModule],
-  controllers: [AppController],
+  controllers: [AppController, ControllersController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/courses/entities/course.ts
+++ src/courses/entities/course.ts
@@ -2,8 +2,8 @@ export class Course {
   id: number;
   title: string;
   description: string;
-  lessons: string;      // Should be an Lesson[] in the future
-  categories: string;     // Should be an Category[] in the future
+  lessons: string; // Should be an Lesson[] in the future
+  categories: string; // Should be an Category[] in the future
   keywords: string;
   state: string;
 }
