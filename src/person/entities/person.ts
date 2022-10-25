190b8a9c9f0d3909dde7984515edee79ed753b1b
Guillermo Jimenez <jimenezguillermo1208@gmail.com>
2022-10-25 09:48:20 -0400

    agrego unos campos a persona de control

--- src/person/entities/person.ts
+++ src/person/entities/person.ts
@@ -1,20 +1,45 @@
 import { Course } from 'src/courses/entities/course';
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Person {
   @PrimaryGeneratedColumn()
   id: number;
+
   @ManyToMany(() => Course, (course) => course.students)
   courses?: Course[];
+
   @Column()
   name: string;
+
   @Column()
   surname: string;
+
   @Column()
   email: string;
+
   @Column()
   phone: number;
+
   @Column()
   isActive: boolean;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
 }
