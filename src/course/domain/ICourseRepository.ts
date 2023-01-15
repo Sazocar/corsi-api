f4a49c84bfa46ec60ffa15620437835ddb04f7b7
Guillermo Jimenez <jimenezguillermo1208@gmail.com>
2023-01-15 16:06:29 -0400

    Modificaciones ICourseRepository

--- src/course/domain/ICourseRepository.ts
+++ src/course/domain/ICourseRepository.ts
@@ -1,11 +1,12 @@
 import { CourseID } from 'src/shared/value_objects/idcourse';
 import { Course } from './course';
 export interface ICourseRepository {
-  createCourse(): void;
   findCourse(course: Course): Course;
   findCourses(course: Course): Array<Course>;
   findCourseByCourseId(courseId: CourseID): Course;
   findPublishedCourses(): Array<Course>;
   findCoursesByCategory(category: string): Array<Course>;
-  findCourseByKeywords(keywords: Array<Keyword>): Array<Course>;
+  findCourseByKeywords(keywords: string): Array<Course>;
+  updateCourse(course: Course): void;
+  deleteCourse(course: Course): void;
 }
