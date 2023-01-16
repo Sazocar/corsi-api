d23642bbb31afa043a4d2f0e3602a851214bbbd3
Simón A Azócar <saimn.azocar@gmail.com>
2023-01-16 14:29:40 -0400

    refactoring in person.ts and students

--- src/User/Domain/Value_Objects/AccountState.ts
+++
@@ -1,4 +0,0 @@
-enum AccountState {
-  ACTIVE,
-  BLOCKED,
-}
---
+++ src/User/Domain/Value_Objects/PersonBirthDate.ts
@@ -0,0 +1,7 @@
+export class PersonBirthDate {
+  //Por los momentos lo coloco como int
+  date: Date;
+  PersonBirthDate(date: Date) {
+    this.date = date;
+  }
+}
